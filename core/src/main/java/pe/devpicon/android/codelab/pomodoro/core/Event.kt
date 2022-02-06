cc48b6e016063d44c377c7bb4d60b596536e1de6
Armando <armando@cornershopapp.com>
2022-02-06 12:23:00 -0500

    Add Event base class

---
+++ core/src/main/java/pe/devpicon/android/codelab/pomodoro/core/Event.kt
@@ -0,0 +1,61 @@
+package pe.devpicon.android.codelab.pomodoro.core
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+/**
+ * Used as a wrapper for data that is exposed via a LiveData that represents an event.
+ * https://medium.com/androiddevelopers/livedata-with-snackbar-navigation-and-other-events-the-singleliveevent-case-ac2622673150
+ */
+open class Event<out T>(private val content: T) {
+
+    private var hasBeenHandled = false
+        private set // Allow external read but not write
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    /**
+     * Returns the content, even if it's already been handled.
+     */
+    fun peekContent(): T = content
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is Event<*>) {
+            other.content == this.content
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return content.hashCode()
+    }
+
+    override fun toString(): String {
+        return content.toString()
+    }
+}
+
+/**
+ * An [Observer] for [Event]s, simplifying the pattern of checking if the [Event]'s content has
+ * already been handled.
+ *
+ * [onEventUnhandledContent] is *only* called if the [Event]'s contents has not been handled.
+ */
+inline fun <T> LiveData<Event<T>>.observeEvent(
+    owner: LifecycleOwner,
+    crossinline onEventUnhandledContent: (T) -> Unit
+) {
+    observe(owner) { it?.getContentIfNotHandled()?.let(onEventUnhandledContent) }
+}
