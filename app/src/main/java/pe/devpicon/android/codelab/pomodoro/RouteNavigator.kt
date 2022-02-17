31d31cc0e89a36eace4c8fa462df653d075a6423
Armando <armando@cornershopapp.com>
2022-02-17 09:02:58 -0500

    configure navigation from login to task

--- app/src/main/java/pe/devpicon/android/codelab/pomodoro/RouteNavigator.kt
+++ app/src/main/java/pe/devpicon/android/codelab/pomodoro/RouteNavigator.kt
@@ -1,15 +1,22 @@
 package pe.devpicon.android.codelab.pomodoro
 
 import androidx.navigation.NavController
+import pe.devpicon.android.codelab.pomodoro.login.LoginFragmentDirections
+import pe.devpicon.android.codelab.pomodoro.login.LoginNavigator
+import javax.inject.Inject
 
-class RouteNavigator {
-    private var navController:NavController? = null
+object RouteNavigator : LoginNavigator {
+    private var navController: WeakReference<NavController>? = null
 
-    fun bind(navController: NavController){
-        this.navController = navController
+    fun bind(nav: NavController) {
+        this.navController = WeakReference<NavController>(nav)
     }
 
-    fun unbind(){
+    fun unbind() {
         this.navController = null
+    }
+
+    override fun navigateOnLoginSuccess() {
+        navController?.navigate(LoginFragmentDirections.actionLoginFragmentToTaskListFragment())
     }
 }
--- app/src/main/java/pe/devpicon/android/codelab/pomodoro/app/AppModule.kt
+++ app/src/main/java/pe/devpicon/android/codelab/pomodoro/app/AppModule.kt
@@ -1,6 +1,25 @@
 package pe.devpicon.android.codelab.pomodoro.app
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import pe.devpicon.android.codelab.pomodoro.RouteNavigator
+import pe.devpicon.android.codelab.pomodoro.login.LoginNavigator
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideLoginNavigator(): LoginNavigator = RouteNavigator
+
+    @Singleton
+    @Provides
+    fun provideRouteNavigator(): RouteNavigator = RouteNavigator
 
 }
 
