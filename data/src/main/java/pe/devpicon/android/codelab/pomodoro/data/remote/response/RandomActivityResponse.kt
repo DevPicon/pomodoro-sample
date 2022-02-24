61ed461c006e5a411936b00f02ebb8daa51fa159
Armando <armando@cornershopapp.com>
2022-02-24 12:13:48 -0500

    fix data type

--- data/src/main/java/pe/devpicon/android/codelab/pomodoro/data/remote/response/RandomActivityResponse.kt
+++ data/src/main/java/pe/devpicon/android/codelab/pomodoro/data/remote/response/RandomActivityResponse.kt
@@ -1,11 +1,11 @@
 package pe.devpicon.android.codelab.pomodoro.data.remote.response
 
 data class RandomActivityResponse(
-    val accessibility: Int,
+    val accessibility: Double,
     val activity: String,
     val key: String,
     val link: String,
     val participants: Int,
-    val price: Int,
+    val price: Double,
     val type: String
 )
