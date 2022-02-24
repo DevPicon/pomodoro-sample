6bd3dc515ed30104e6e17ad5064b4214156baf74
Armando <armando@cornershopapp.com>
2022-02-24 12:05:24 -0500

    implement random activity generation

--- features/task/src/main/java/pe/devpicon/android/codelab/pomodoro/task/create/CreateTaskViewModel.kt
+++ features/task/src/main/java/pe/devpicon/android/codelab/pomodoro/task/create/CreateTaskViewModel.kt
@@ -10,6 +10,7 @@ import pe.devpicon.android.codelab.pomodoro.core.Event
 import pe.devpicon.android.codelab.pomodoro.core.SnackBarError
 import pe.devpicon.android.codelab.pomodoro.domain.common.Error
 import pe.devpicon.android.codelab.pomodoro.domain.common.ResultWrapper
+import pe.devpicon.android.codelab.pomodoro.domain.usecase.random.GetRandomActivityUseCase
 import pe.devpicon.android.codelab.pomodoro.domain.usecase.task.CreateTaskUseCase
 import java.util.*
 import javax.inject.Inject
@@ -17,7 +18,8 @@ import javax.inject.Inject
 @HiltViewModel
 class CreateTaskViewModel
 @Inject constructor(
-    private val createTaskUseCase: CreateTaskUseCase
+    private val createTaskUseCase: CreateTaskUseCase,
+    private val getRandomActivityUseCase: GetRandomActivityUseCase
 ) : ViewModel() {
     private var taskName: String = ""
     private var taskPomodoroQty: Int = 1
@@ -45,7 +47,39 @@ class CreateTaskViewModel
             is CreateTaskScreenEvent.SaveTask -> {
                 save()
             }
+            CreateTaskScreenEvent.OnPressRandomButton -> getRandomActivity()
         }
+    }
+
+    private fun getRandomActivity() {
+        val startState = getStartState()
+        startState?.let {
+            val showInlineLoader = it.copy(showInlineLoader = true)
+            _screenState.value = Event(showInlineLoader)
+            viewModelScope.launch {
+                when (val result = getRandomActivityUseCase(Unit)) {
+                    is ResultWrapper.Failure -> {
+                        onFailure(result.error)
+                        _screenState.value = Event(it.copy(showInlineLoader = false))
+                    }
+                    is ResultWrapper.Success -> {
+                        setTaskName(result.value)
+                        val newContent = it.copy(name = result.value, showInlineLoader = false)
+                        _screenState.value = Event(newContent)
+                    }
+                }
+            }
+        } ?: run {
+            onFailure(Error.GenericError(message = "Invalid state"))
+        }
+    }
+
+    private fun getStartState(): CreateTaskScreenState.Start? {
+        var currentState: CreateTaskScreenState.Start? = null
+        if (_screenState.value?.peekContent() is CreateTaskScreenState.Start) {
+            currentState = _screenState.value?.peekContent() as CreateTaskScreenState.Start
+        }
+        return currentState
     }
 
     private fun save() {
