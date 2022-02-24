c4fbc2feaec1428f63a2ba66a2d1bf894b60ed96
Armando <armando@cornershopapp.com>
2022-02-24 07:14:56 -0500

    add worker

---
+++ sync/src/main/java/pe/devpicon/android/codelab/pomodoro/sync/workmanager/TaskWorkerFactory.kt
@@ -0,0 +1,30 @@
+package pe.devpicon.android.codelab.pomodoro.sync.workmanager
+
+import android.content.Context
+import androidx.work.ListenableWorker
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
+import pe.devpicon.android.codelab.pomodoro.data.local.UserLocalDataSource
+import pe.devpicon.android.codelab.pomodoro.data.remote.TaskRemoteDataSource
+import pe.devpicon.android.codelab.pomodoro.data.sync.SyncErrorHandler
+import pe.devpicon.android.codelab.pomodoro.sync.workmanager.workers.InsertTaskWorker
+
+class TaskWorkerFactory(
+    private val userLocalDataSource: UserLocalDataSource,
+    private val taskRemoteDataSource: TaskRemoteDataSource,
+    private val errorHandler: SyncErrorHandler
+) : WorkerFactory() {
+    override fun createWorker(
+        appContext: Context,
+        workerClassName: String,
+        workerParameters: WorkerParameters
+    ): ListenableWorker? {
+        return when (workerClassName) {
+            InsertTaskWorker::class.java.name -> InsertTaskWorker(
+                appContext,
+                workerParameters, userLocalDataSource, taskRemoteDataSource, errorHandler
+            )
+            else -> null
+        }
+    }
+}
